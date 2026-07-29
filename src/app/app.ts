41939d02656cda92f4d30b4900c5ced2dfcf7501
suraj-6277 <jadhavsuraj246810@gmail.com>
2026-07-29 23:54:12 +0530

    feat: set up application shell

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { PayloadBuilder } from './components/payload-builder/payload-builder';
+import { DocumentExtractor } from './components/document-extractor/document-extractor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [PayloadBuilder, DocumentExtractor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('payload-builder');
-}
+export class App {}
